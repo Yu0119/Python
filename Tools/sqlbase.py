1de4967fb2f3c8f1bc6122b112496c0bd0e03510
Yu0119 <yuu.hasegawa119@gmail.com>
2018-02-10 01:50:51 +0900

    add sqlbase.py

---
+++ Tools/sqlbase.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+ Brief: Base code for use sqlalchemy
+"""
+from __future__ import print_function
+import sqlalchemy
+import sqlalchemy.ext.declarative
+import sqlalchemy.orm
+
+
+eng = sqlalchemy.create_engine('sqlite:///:memory:') # 
+Decbase = sqlalchemy.ext.declarative.declarative_base()
+
+
+# Item class
+class Item(Decbase):
+  """
+    Brief:
+      Item Database class
+    Extends:
+      Decbase
+  """
+  # Table名称
+  __tablename__ = 'items'
+  # カラムのID
+  column_id = sqlalchemy.Column(
+    sqlalchemy.Integer, primary_key=True, autoincrement=True
+  )
+  # ITEM名称(string)
+  name = sqlalchemy.Column(sqlalchemy.String(30))
+  price = sqlalchemy.Column(sqlalchemy.INTEGER())
+
+
+Decbase.metadata.create_all(eng)
+
+Session = sqlalchemy.orm.sessionmaker(bind=eng)
+session = Session()
+
+item1 = Item(name='ice-cream', price=150)
+session.add(item1)
+item2 = Item(name='candy', price=50)
+session.add(item2)
+
+# Add items ...
+
+# Commit set items
+session.commit()
+
+# Search all queries
+items = session.query(Item).all()
+
+for item in items:
+  print(item.column_id, item.name, item.price)
+  
