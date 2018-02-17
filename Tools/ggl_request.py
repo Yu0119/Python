ccfc1a94d2859f4551993569e34279d5201cc040
Yu0119 <yuu.hasegawa119@gmail.com>
2018-02-17 13:13:20 +0900

    first commit

---
+++ Tools/ggl_request.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""[summary]: Make google search query with urllib
+"""
+import webbrowser
+import urllib
+import urllib.request
+import json
+
+payload = { 'q': 'python', 'oq': 'python' }
+
+url = 'https://google.co.jp/search' + '?' + \
+        urllib.parse.urlencode(payload)
+
+print(url)
+webbrowser.open(url)
