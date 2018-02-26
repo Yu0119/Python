22eb525853eaa610dd215850f8ba2f1ade836d9d
YH_MLeng <yuu.hasegawa119@gmail.com>
2018-02-27 08:10:43 +0900

    Update models.py

--- django/RestApiSample/app/models.py
+++ django/RestApiSample/app/models.py
@@ -2,8 +2,14 @@ from django.db import models
 
 class User(models.Model):
   name = models.CharField(max_length=64)
+  
+  def __str__(self):
+    return self.name
 
 class Article(models.Model):
   user = models.ForeignKey(User, related_name='article')
   title = models.CharField(max_length=1024)
   contents = models.TextField()
+  
+  def __str__(self):
+    return self.title
