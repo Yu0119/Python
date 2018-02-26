8be45889dc1abcb17eec19ea82af158cbcd48133
YH_MLeng <yuu.hasegawa119@gmail.com>
2018-02-27 07:41:15 +0900

    Update settings.py

--- django/RestApiSample/RestApiSample/settings.py
+++ django/RestApiSample/RestApiSample/settings.py
@@ -85,8 +85,6 @@ DATABASES = {
 
 
 # Password validation
-# https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -104,7 +102,6 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 # Internationalization
-# https://docs.djangoproject.com/en/1.9/topics/i18n/
 
 # LANGUAGE_CODE = 'en-us'
 LANGUAGE_CODE = 'ja'
