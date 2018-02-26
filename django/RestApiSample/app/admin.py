0404a8d24850420d30c62272adf54a61ebfd0eb9
YH_MLeng <yuu.hasegawa119@gmail.com>
2018-02-26 22:32:41 +0900

    Update admin.py

--- django/RestApiSample/app/admin.py
+++ django/RestApiSample/app/admin.py
@@ -1,1 +1,11 @@
 from django.contrib import admin
+
+from .models import User, Article
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+  pass
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+  pass
