1e38aef363650464dcdb51dd51e15756c55567d2
YH_MLeng <yuu.hasegawa119@gmail.com>
2018-03-10 09:01:16 +0900

    Create pytorch_autoencoder.py

---
+++ MLs/pytorch_autoencoder.py
@@ -0,0 +1,110 @@
+"""
+ Pytorch Autoencoder
+"""
+import torch
+import torch.nn as nn
+import torchvision
+import torchvision.models as models
+import torchvision.transforms as transforms
+from torch.autograd import Variable
+from torchvision.utils import save_image
+
+import os
+import six
+
+
+# Convert to image arrays
+def to_img(x):
+    x = 0.5 * (x + 1)
+    x = x.clamp(0, 1)
+    x = x.view(x.size(0), 1, 28, 28)
+    return x
+
+
+# Autoencoder Model
+class AutoEncoder(nn.Module):
+    
+    def __init__(self):
+        super(AutoEncoder, self).__init__()
+        self.encoder = nn.Sequential(
+            nn.Linear(784, 64),
+            nn.Tanh(),
+            nn.Linear(64, 8),
+            nn.Tanh(),
+            nn.Linear(8, 3)
+        )
+        self.decoder = nn.Sequential(
+            nn.Linear(3, 8),
+            nn.Tanh(),
+            nn.Linear(8, 64),
+            nn.Tanh(),
+            nn.Linear(64, 784)
+        )
+        
+    def forward(self, x):
+        encoded = self.encoder(x)
+        decoded = self.decoder(encoded)
+        
+        return encoded, decoded
+        
+
+if __name__ == '__main__':
+
+  # Hyper parameters
+  num_epochs = 30
+  input_size = 28*28
+  learning_rate = 0.001
+
+  # Datasets
+  images = torchvision.datasets.FashionMNIST(
+      root='./data',
+      train=True,
+      transform=transforms.ToTensor(),
+      download=True
+  )
+  train_loader = torch.utils.data.DataLoader(
+      images,
+      shuffle=True,
+      batch_size=batch_size
+  )
+
+  # Create model
+  autoencoder = AutoEncoder()
+  print(autoencoder)
+
+  import torch.optim as optim
+
+  # Optimizer
+  criterion = nn.MSELoss()
+  optimizer = optim.Adam(autoencoder.parameters(), lr=learning_rate, weight_decay=1e-5)
+
+  # Create output directory
+  if not os.path.exists('output'): os.mkdir('output')
+
+  # Train    
+  for epoch in six.moves.range(num_epochs):
+
+      sum_loss = 0.0
+      for i, (images, _) in enumerate(train_loader):
+          dtype = torch.FloatTensor
+          # datasets
+          images = Variable(images.view(-1, 28*28).type(dtype))
+
+          optimizer.zero_grad()
+
+          # forward & backprop
+          _, encoded = autoencoder(images)
+          loss = criterion(encoded, images)
+          loss.backward()
+          optimizer.step()
+
+          sum_loss += loss.data[0]
+
+          if (i+1) % 100 == 0:
+              print('Epoch [{}/{}], Step [{}], loss: {}'.format(epoch+1, num_epochs, i+1, sum_loss))
+
+              # Initialize sum loss
+              sum_loss = 0.0
+
+      pic = to_img(encoded.cpu().data)
+      save_image(pic, './output/image_epoch{}.png'.format(epoch))
